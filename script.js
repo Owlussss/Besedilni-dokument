b752eed44f5cc45051cccd0c60b8075902057512
A2 <184486074+yrhn2tb@users.noreply.github.com>
2024-11-29 09:39:58 +0100

    add a check statement

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // dark mode toggle
 const darkModeButton = document.getElementById("dark-mode-btn");
-const nav = document.getElementsByTagName("nav");
+const nav = document.getElementById("nav");
 
 darkModeButton.addEventListener('click', () => {
     document.body.classList.toggle("dark-mode");
-})
+
+    // check which mode the website is in right now
+    if (document.body.classList.contains("dark-mode")) {
+        nav.style.backgroundColor = "#";
+    } else {
+        nav.style.backgroundClip = "#";
+    }
+});
 
