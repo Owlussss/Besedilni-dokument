8270037f4355dd93b82c539bfb09520ec70eedd7
A2 <184486074+yrhn2tb@users.noreply.github.com>
2024-12-04 21:27:40 +0100

    aaaaaa

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // 1) dark mode toggle
 const darkModeButton = document.getElementById("darkmode");
-const body = document.body; // Target the body to toggle dark mode
+const body = document.body;
 
 darkModeButton.addEventListener('click', () => {
-    // Toggle the dark-mode class on the body element
     body.classList.toggle("dark-mode");
 });
 
