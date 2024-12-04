c416665e534eb2229767995d1c7d7f6477288c35
A2 <184486074+yrhn2tb@users.noreply.github.com>
2024-12-04 21:17:08 +0100

    aa

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 // 1) dark mode toggle
-const darkModeButton = document.getElementById("nightmode");
-const nav = document.getElementById("nav");
+const darkModeButton = document.getElementById("darkmode");
+const body = document.body; // Target the body to toggle dark mode
 
 darkModeButton.addEventListener('click', () => {
-    document.body.classList.toggle("dark-mode");
-
-    // check which mode the website is in right now
-    if (document.body.classList.contains("dark-mode")) {
-        nav.style.backgroundColor = "#";
-    } else {
-        nav.style.backgroundClip = "#";
-    }
+    // Toggle the dark-mode class on the body element
+    body.classList.toggle("dark-mode");
 });
 
 // 2) show date
